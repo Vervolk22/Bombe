4571d4b5ddf02861426f1128342ddc3e7af9584d
Andrei Zholudzeu <Bozon22@gmail.com>
2015-12-07 23:51:55 +0300

    Added ComputingHelpers settings class

---
+++ ComputingHelpers/LibrarySettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputingHelpers
+{
+    internal static class LibrarySettings
+    {
+        internal static string[] rotorsLayout = {
+                                                   "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+                                                   "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+                                                   "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+                                                   "NQDJXVLSPHUFACKOIYRWZMEBGT",
+                                                   "CKPESOHXVUMJRFYALGQBTIDZWN",
+                                                   "PGUYIOTMBXKFAHVRLZDNSWECJQ",
+                                                   "YRUHQSLDPXNGOKMIEBFZCWVJAT",
+                                                   "TBKOZSREHXUCPNWFLGDVQIJAYM",
+                                                   "VWFGYDTNQUEHRMZXOBSAVLJPKI",
+                                                   "RKSLQNWCGDTHYAOEUMFZBVPXIJ",
+                                                   "PEQJMVFTYRHLDCSIWNAUZGOXBK"
+                                               };
+        internal static char[] notchPositions = { 'V', 'E', 'Q', 'D', 'W', 'O', 'A', 'U', 'L', 'N' };
+    }
+}
