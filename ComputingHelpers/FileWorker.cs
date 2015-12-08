44d1fcb91659725ad94487fd4871879b5d003503
Andrei Zholudzeu <Bozon22@gmail.com>
2015-12-08 14:33:06 +0300

    Added layout and notch validation to FileWorker

--- ComputingHelpers/FileWorker.cs
+++ ComputingHelpers/FileWorker.cs
@@ -35,6 +35,14 @@ namespace ComputingHelpers
                     settings[i] = reader.ReadLine();
                 }
                 reader.Close();
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (!checkLayout(settings[i * 2]) || !checkNotch(settings[i * 2 + 1]))
+                        return writeSettingsFile();
+                }
+                if (!checkLayout(settings[20]))
+                    return writeSettingsFile();
                 return settings;
             }
             catch (IOException e)
@@ -49,28 +57,57 @@ namespace ComputingHelpers
             string[] settings = new string[21];
             for (int i = 0; i < 10; i++)
             {
-                settings[i * 2] = LibrarySettings.rotorsLayout[i];
-                settings[i * 2 + 1] = LibrarySettings.notchPositions[i].ToString();
+                settings[i * 2] = Settings.rotorsLayout[i];
+                settings[i * 2 + 1] = Settings.notchPositions[i].ToString();
                 writer.WriteLine(settings[i * 2]);
                 writer.WriteLine(settings[i * 2 + 1]);
             }
-            settings[20] = LibrarySettings.rotorsLayout[10];
+            settings[20] = Settings.rotorsLayout[10];
             writer.WriteLine(settings[20]);
             writer.Flush();
             writer.Close();
             return settings;
         }
 
-        protected string getAppSetting(Configuration config, string key)
+        public static bool checkLayout(string s)
         {
-            KeyValueConfigurationElement element = config.AppSettings.Settings[key];
-            if (element != null)
+            if (s.Length != Settings.ALPHABET_LENGTH) return false;
+            byte[] array = new byte[Settings.ALPHABET_LENGTH];
+            int counter = 0;
+            try
             {
-                string value = element.Value;
-                if (!string.IsNullOrEmpty(value))
-                    return value;
+                foreach (char ch in s)
+                {
+                    if (array[ch - 'A'] == 0)
+                    {
+                        counter++;
+                        array[ch - 'A']++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                if (counter == Settings.ALPHABET_LENGTH)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
-            return string.Empty;
+            catch (Exception e)
+            {
+                return false;
+            }
+
+        }
+
+        public static bool checkNotch(string s)
+        {
+            if (s.Length == 1 && (char)s[0] >= 'A' && (char)s[0] <= 'Z') return true;
+            else return false;
         }
     }
 }
